fbccf8a3ae84200c2fa65fbaaba8a818d94b1d73
Ning Ding <shdn@cn.ibm.com>
2021-07-02 20:24:50 +0800

    Update status-card.tsx

--- src/components/status-card/status-card.tsx
+++ src/components/status-card/status-card.tsx
@@ -16,10 +16,6 @@
 import * as React from 'react';
 import * as _ from 'lodash';
 import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardTitle,
   Gallery,
   GalleryItem,
 } from "@patternfly/react-core";
@@ -27,11 +23,14 @@ import {
   useK8sWatchResource,
 } from "@console/dynamic-plugin-sdk/api";
 import {
+  DashboardCard,
+  DashboardCardBody,
+  DashboardCardHeader,
+  DashboardCardTitle,
   HealthItem,
 } from "@console/dynamic-plugin-sdk/provisional";
 import { getFlashsystemHealthState } from './utils';
 import { StorageInstanceKind } from '../../types';
-//import { StorageInstanceModel } from '../../../../models';
 import {GetFlashSystemResource} from '../../constants/resources'
 
 export const StatusCard: React.FC<any> = (props) => {
@@ -39,24 +38,22 @@ export const StatusCard: React.FC<any> = (props) => {
   const flashHealthState = getFlashsystemHealthState({ sto: { data: data, loaded: loaded, loadError: loadError } });
 
   return (
-    <Card className="co-dashboard-card co-dashboard-card--gradient">
-      <CardHeader className="co-dashboard-card__header">
-        <CardTitle className="co-dashboard-card__title">Status</CardTitle>
-      </CardHeader>
-      <CardBody className="co-dashboard-card__body">
-        <div className="co-dashboard-card__body--top-margin co-status-card__health-body">
-          <Gallery className="co-overview-status__health" hasGutter>
-            <GalleryItem>
-              <HealthItem
-                title={props?.match?.params?.name}
-                state={flashHealthState.state}
-                details={flashHealthState.message}
-              />
-            </GalleryItem>
-          </Gallery>
-        </div>
-      </CardBody>
-    </Card>
+    <DashboardCard gradient>
+      <DashboardCardHeader>
+        <DashboardCardTitle>Status</DashboardCardTitle>
+      </DashboardCardHeader>
+      <DashboardCardBody>
+        <Gallery className="co-overview-status__health" hasGutter>
+          <GalleryItem>
+            <HealthItem
+              title={props?.match?.params?.name}
+              state={flashHealthState.state}
+              details={flashHealthState.message}
+            />
+          </GalleryItem>
+        </Gallery>
+      </DashboardCardBody>
+    </DashboardCard>
   );
 };
 
