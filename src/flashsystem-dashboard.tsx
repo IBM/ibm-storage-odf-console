e1263d6e6084e3233f2f03c6c1136de743289656
Ning Ding <shdn@cn.ibm.com>
2021-07-24 16:57:07 +0800

    fix: format code

--- src/flashsystem-dashboard.tsx
+++ src/flashsystem-dashboard.tsx
@@ -13,91 +13,91 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- import * as React from 'react';
- import * as _ from 'lodash';
- import { RouteComponentProps } from "react-router";
- import { HorizontalNav } from "@console/dynamic-plugin-sdk/internalAPI";
- import { Grid, GridItem } from "@patternfly/react-core";
- 
- import StorageEfficiencyCard from './components/storage-efficiency-card/storage-efficiency-card';
- import StatusCard from './components/status-card/status-card';
- import DetailsCard from './components/details-card/details-card';
- import InventoryCard from './components/inventory-card/inventory-card';
- import ActivityCard from './components/activity-card/activity-card';
- import RawCapacityCard from './components/raw-capacity-card/raw-capacity-card';
- import UtilizationCard from './components/utilization-card/utilization-card';
- import BreakdownCard from './components/capacity-breakdown/capacity-breakdown-card';
+import * as React from 'react';
+import * as _ from 'lodash';
+import { RouteComponentProps } from "react-router";
+import { HorizontalNav } from "@console/dynamic-plugin-sdk/internalAPI";
+import { Grid, GridItem } from "@patternfly/react-core";
 
- export type ODFDashboardProps = {
+import StorageEfficiencyCard from './components/storage-efficiency-card/storage-efficiency-card';
+import StatusCard from './components/status-card/status-card';
+import DetailsCard from './components/details-card/details-card';
+import InventoryCard from './components/inventory-card/inventory-card';
+import ActivityCard from './components/activity-card/activity-card';
+import RawCapacityCard from './components/raw-capacity-card/raw-capacity-card';
+import UtilizationCard from './components/utilization-card/utilization-card';
+import BreakdownCard from './components/capacity-breakdown/capacity-breakdown-card';
+
+export type ODFDashboardProps = {
   match: RouteComponentProps["match"];
 };
 
- const UpperSection: React.FC = (props) => {
+const UpperSection: React.FC = (props) => {
+return (
+  <Grid hasGutter>
+    <GridItem span={3}>
+      <Grid hasGutter>
+        <GridItem>
+          <DetailsCard/>
+        </GridItem>
+        <GridItem>
+          <StorageEfficiencyCard/>
+        </GridItem>          
+        <GridItem>
+          <InventoryCard/>
+        </GridItem>
+      </Grid>
+    </GridItem>
+    <GridItem span={6}>
+      <Grid hasGutter>
+        <GridItem>
+          <StatusCard {...props}/>
+        </GridItem>
+        <GridItem>
+          <RawCapacityCard {...props}/>
+        </GridItem>
+        <GridItem>
+          <BreakdownCard {...props}/>
+        </GridItem>
+        <GridItem>
+          <UtilizationCard {...props}/>
+        </GridItem>
+      </Grid>         
+    </GridItem>
+    <GridItem span={3} >
+      <Grid hasGutter>
+        <GridItem>
+          <ActivityCard/>
+        </GridItem>
+      </Grid>
+    </GridItem>
+  </Grid>
+);
+};
+
+const FlashsystemDashboard: React.FC<ODFDashboardProps> = (props) => {
   return (
-    <Grid hasGutter>
-      <GridItem span={3}>
-        <Grid hasGutter>
-          <GridItem>
-            <DetailsCard/>
-          </GridItem>
-          <GridItem>
-            <StorageEfficiencyCard/>
-          </GridItem>          
-          <GridItem>
-            <InventoryCard/>
-          </GridItem>
-        </Grid>
-      </GridItem>
-      <GridItem span={6}>
-        <Grid hasGutter>
-          <GridItem>
-            <StatusCard {...props}/>
-          </GridItem>
-          <GridItem>
-            <RawCapacityCard {...props}/>
-          </GridItem>
-          <GridItem>
-            <BreakdownCard {...props}/>
-          </GridItem>
-          <GridItem>
-            <UtilizationCard {...props}/>
-          </GridItem>
-        </Grid>         
-      </GridItem>
-      <GridItem span={3} >
-        <Grid hasGutter>
-          <GridItem>
-            <ActivityCard/>
-          </GridItem>
-        </Grid>
-      </GridItem>
-    </Grid>
+    <>
+      <div className="co-dashboard-body">
+        <UpperSection {...props}/>
+      </div>
+    </>
   );
- };
- 
- const FlashsystemDashboard: React.FC<ODFDashboardProps> = (props) => {
-   return (
-     <>
-       <div className="co-dashboard-body">
-         <UpperSection {...props}/>
-       </div>
-     </>
-   );
- };
- 
- const FlashsystemDashboardPage: React.FC<ODFDashboardProps> = ({ match }) => {
-   const allPages = [
-     {
-       href: "",
-       name: "Overview",
-       component: FlashsystemDashboard,
-     },
-   ];
-   return (
-     <>
-       <HorizontalNav match={match} pages={allPages} noStatusBox />
-     </>
-   );
- };
- 
- export default FlashsystemDashboardPage;
+};
+
+const FlashsystemDashboardPage: React.FC<ODFDashboardProps> = ({ match }) => {
+  const allPages = [
+    {
+      href: "",
+      name: "Overview",
+      component: FlashsystemDashboard,
+    },
+  ];
+  return (
+    <>
+      <HorizontalNav match={match} pages={allPages} noStatusBox />
+    </>
+  );
+};
+
+export default FlashsystemDashboardPage;
