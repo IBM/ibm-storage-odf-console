89e2d203be0e220616c7279b40f377938babd363
662962756 <bvered@il.ibm.com>
2022-11-28 14:39:19 +0200

    edit PVC label
    Signed-off-by: 662962756 <bvered@il.ibm.com>

--- src/components/capacity-breakdown/capacity-breakdown-card.tsx
+++ src/components/capacity-breakdown/capacity-breakdown-card.tsx
@@ -98,7 +98,6 @@ const BreakdownCard: React.FC<any> = (props) => {
     samples: 60,
   });
   const PVCsWithoutStorage = _.get(countPVCsWithoutStorage, "data.result[0].value[1]");
-  console.log("my pvc without storage count it %v", PVCsWithoutStorage)
   if (PVCsWithoutStorage > 0 ){
     WarningMessage = "* " +  t('Provisioned capacity might be inaccurate as some PVCs are not properly associated with a specific storage system.')
     PVCWarning = true
