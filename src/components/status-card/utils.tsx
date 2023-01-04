c791bb1059f51bc5ab03d5ccacb9aaa40f673cdb
alonfirestein <alon@firestein.net>
2023-01-04 09:24:04 +0200

    removed unused filter function from utils.tsx file
    Signed-off-by: alonfirestein <alon@firestein.net>

--- src/components/status-card/utils.tsx
+++ src/components/status-card/utils.tsx
@@ -22,7 +22,6 @@ import {
   Rule,
 } from "@openshift-console/dynamic-plugin-sdk-internal/lib/api/common-types";
 import { StorageInstanceKind } from "../../types";
-import { IBM_FLASHSYSTEM } from "../../constants/constants";
 
 enum HealthState {
   OK = "OK",
@@ -98,12 +97,6 @@ export const labelsToParams = (labels: PrometheusLabels) =>
 export const alertURL = (alert: Alert, ruleID: string) =>
   `${AlertResource.plural}/${ruleID}?${labelsToParams(alert.labels)}`;
 
-export const filterIBMFlashSystemAlerts = (alerts: Alert[]): Alert[] =>
-  alerts.filter(
-    (alert) =>
-      _.get(alert, "annotations.storage_type")?.toLowerCase() ===
-        IBM_FLASHSYSTEM.toLowerCase()
-  );
 
 export const getAlertsFromPrometheusResponse = (
   response: PrometheusRulesResponse
